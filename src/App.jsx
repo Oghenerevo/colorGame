615f8c134b37a50548180184c90946e8e056abc5
Justfemi <ajibade.olufemi@yahoo.com>
2025-02-03 02:24:08 +0100

    feat:completed game home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className="container">
+      <header>
+        <h1>Color<span>Match</span></h1>
+
+        <div className="bg-score">
+          <h3>Score: <strong>0</strong> </h3>
+        </div>
+
+        <div className='header-btns'>
+          <div>
+            <button>Help</button>
+          </div>
+          
+          <div>
+            <button>Restart</button>
+          </div>
+        </div>
+      </header>
+      
+      {/* score for small screens */}
+      <div className="sm-score">
+        <h3>Score: <strong>0</strong> </h3>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <section className="game-board">
+        <div>
+          <div className="target-color"></div>
+        </div>
+
+        <div className="options">
+          <div className="option-color one"></div>
+          <div className="option-color two"></div>
+          <div className="option-color three"></div>
+          <div className="option-color four"></div>
+          <div className="option-color five"></div>
+          <div className="option-color six"></div>
+        </div>
+      </section>
+
+      <footer>
+        <p>&copy; 2025 Revo. All rights reserved.</p>
+      </footer>
+    </section>
   )
 }
 
